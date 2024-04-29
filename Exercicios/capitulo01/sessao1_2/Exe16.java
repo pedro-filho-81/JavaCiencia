b49291d7a9ac72b89b4b2122b89a7ca25dc09df2
Pedro Filho <psfilho81@gmail.com>
2024-04-29 17:24:59 -0300

    Create Exe16.java

---
+++ Exercicios/capitulo01/sessao1_2/Exe16.java
@@ -0,0 +1,26 @@
+package sessao1_2;
+
+import java.util.Scanner; // for input
+
+/*
+ * 1.2.16 Um estudante de física obtém resultados 
+ * inesperados ao usar o código Clique aqui para 
+ * visualizar a imagem do código double force = G * 
+ * mass1 * mass2 / r * r; para calcular valores de 
+ * acordo com a fórmula F = Gm1m2 / r2. Explique o 
+ * problema e corrija o código.
+ */
+public class Exe16 {
+   public static void main(String[] args) {
+      // create object input
+      Scanner input = new Scanner(System.in);
+
+      // create variable
+      double g; // força G
+      double mass1; // massa1
+      double mass2; // mass
+      double velocidade; // speed
+
+      System.out.println(6.4 * Math.pow(10, 6));
+   }
+}
